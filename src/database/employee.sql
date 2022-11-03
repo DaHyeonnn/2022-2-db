1139f243e02d715ad8b23c8638ce665db375483e
DAHYEONNN <ekgus8742@naver.com>
2022-11-03 15:41:53 +0900

    week10 upload

---
+++ src/database/employee.sql
@@ -0,0 +1,10 @@
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Sex, Salary, Dno) VALUES ("이쩡연", " ", "한", 888777555, "M", 100, 1);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Sex, Salary, Dno) VALUES ("웜밈잼", " ", "우", 987654321, "M", 100, 4);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Sex, Salary, Dno) VALUES ("다현공주", " ", "신", 333444555, "F", 99999, 5);
+
+
+-- INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Sex, Salary, Dno) VALUES ("이은", " ", "선", 188969970, "F", 84582, 4);
+-- INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Sex, Salary, Dno) VALUES ("표조", " ", "배", 274411269, "F", 70001, 4);
+-- INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Sex, Salary, Dno) VALUES ("혜표", " ", "국", 611577491, "F", 54604, 5);
+-- INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Sex, Salary, Dno) VALUES ("홍한", " ", "정", 333010827, "M", 82870, 1);
+-- INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Sex, Salary, Dno) VALUES ("종조", " ", "도", 268317882, "F", 15378, 5);
